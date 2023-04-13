a5f2e6a6d8df917249d79a5b78e721733f67fb0a
Merveil20 <djudjemerveil20@gmail.com>
2023-04-13 15:59:49 +0100

    Dêpot initial

---
+++ src/pages/About.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation/>
+            <h1>A Propos</h1>
+            <br />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Minima eligendi ratione modi laborum voluptatum. Impedit sint harum possimus, exercitationem officiis perspiciatis eveniet rerum fugit nihil est vel, dolorem saepe.
+                Ullam sunt architecto molestias quasi. Autem similique officia, adipisci delectus vitae modi libero maxime corporis fugiat repellat. Quod, neque.
+                Illum eaque, qui nostrum nulla culpa saepe earum ratione quia molestiae odit ex laborum. Veritatis possimus accusamus beatae laboriosam, rem culpa laudantium quae.
+                Iste quasi nostrum incidunt corporis adipisci quis magni, minus repellat nihil deleniti ducimus possimus fuga? Reiciendis earum necessitatibus vero sit dolorem officiis ipsum unde repellat.
+                Repudiandae incidunt quas nam?
+            </p>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Odio eveniet harum ullam illo natus nulla ea, cumque maiores voluptas reprehenderit, nisi numquam sapiente porro molestias qui soluta exercitationem velit.
+                Debitis, tenetur aliquam harum deleniti asperiores vitae nam animi suscipit, voluptates nulla, tempora consectetur cumque hic porro provident veritatis inventore sint corporis veniam? Exercitationem quis sequi laudantium itaque placeat dolore minus doloribus eveniet,
+                nihil sint blanditiis fugit ipsam rem autem dicta! Sit ipsa nihil atque quis tenetur voluptate ea, facere saepe fugit, veniam, explicabo labore recusandae quia placeat esse nostrum dolorem veritatis obcaecati inventore eligendi. Ea deserunt quasi possimus sunt velit!
+            </p>
+            
+        </div>
+    );
+};
+
+export default About;
